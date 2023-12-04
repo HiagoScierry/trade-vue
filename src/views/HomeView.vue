c9d034e4fa11d8ce256792111d634599647b5a70
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-12-04 11:29:19 -0300

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center items-center h-screen w-screen">
+        <h1 class="text-2xl">Hello World !</h1>
+    </div>
+</template>
+
+<script >
+    export default {
+        name: 'HomeView',
+    };
+</script>
